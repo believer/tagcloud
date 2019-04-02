1092e9219ae34e48987e8ee25f29dba1201afc01
Rickard Laurin <rickard.laurin@gmail.com>
2019-04-02 17:02:43 +0200

    fix: only run search if the user has entered a query

--- packages/frontend/src/useTweets.js
+++ packages/frontend/src/useTweets.js
@@ -2,7 +2,12 @@ import { useFetch } from 'react-fetch-hook'
 
 export const useTweets = hashtag => {
   const BASE_URL = 'https://tagcloud.willcodefor.beer'
-  const { data, isLoading } = useFetch(`${BASE_URL}/search?hashtag=${hashtag}`)
+  const { data, isLoading } = useFetch(
+    `${BASE_URL}/search?hashtag=${hashtag}`,
+    {
+      depends: [hashtag],
+    }
+  )
 
   return {
     isLoading,
